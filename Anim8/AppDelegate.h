2310851880591b2b75a4ad0b752b095e53d55eaf
Nico Ameghino <nicolas.ameghino@gmail.com>
2014-06-01 14:11:41 -0300

    Initial Commit

---
+++ Anim8/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Anim8
+//
+//  Created by Nicolas Ameghino on 6/1/14.
+//  Copyright (c) 2014 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Anim8/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Anim8
+//
+//  Created by Nicolas Ameghino on 6/1/14.
+//  Copyright (c) 2014 Nicolas Ameghino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
